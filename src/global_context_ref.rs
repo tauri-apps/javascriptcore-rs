2f211ca83d4fca1508f95bcdead7abb81a21d956
Dirk Van Haerenborgh <dirk.vanhaerenborgh@aloxy.io>
2021-11-01 14:28:04 +0100

    regenerate

---
+++ src/global_context_ref.rs
@@ -0,0 +1,28 @@
+use ffi::*;
+use glib::translate::*;
+use std::ptr;
+
+pub struct GlobalContextRef {
+  raw: JSGlobalContextRef,
+}
+
+impl FromGlibPtrNone<JSGlobalContextRef> for GlobalContextRef {
+  unsafe fn from_glib_none(ptr: JSGlobalContextRef) -> Self {
+    GlobalContextRef { raw: ptr }
+  }
+}
+
+impl FromGlibPtrFull<JSGlobalContextRef> for GlobalContextRef {
+  unsafe fn from_glib_full(ptr: JSGlobalContextRef) -> Self {
+    GlobalContextRef { raw: ptr }
+  }
+}
+
+impl<'a> ToGlibPtr<'a, JSGlobalContextRef> for GlobalContextRef {
+  type Storage = ();
+
+  #[inline]
+  fn to_glib_none(&self) -> Stash<'a, JSGlobalContextRef, GlobalContextRef> {
+    Stash(self.raw, ())
+  }
+}
